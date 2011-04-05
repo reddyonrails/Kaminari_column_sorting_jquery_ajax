7bbadf998e7ba524842edef56f6446431522a4a6
Jagan Reddy <reddyonrails@gmail.com>
2011-04-05 14:26:18 -0700

    sorting and ajax with kaminari plugin

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,22 @@
+class SubscribersController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
+  def index
+    @subscribers = Subscriber.order(sort_column + " " + sort_direction).page(params[:page]).per(15)
+  end
+
+  def show
+
+  end
+
+  private
+
+  def sort_column
+    Subscriber.column_names.include?(params[:sort]) ? params[:sort] : "first_name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+end
